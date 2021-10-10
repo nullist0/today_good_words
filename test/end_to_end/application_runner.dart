6aeb56fe3587a8f421290b0e273ce3c6c28020d3
Lee-Null <nullist.null@gmail.com>
2021-10-10 22:34:45 +0900

    add test for phrase bloc

--- test/end_to_end/application_runner.dart
+++ test/end_to_end/application_runner.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todaygoodwords/main.dart';
+import 'package:todaygoodwords/phrase_themes/phrase_theme.dart';
 import 'package:todaygoodwords/phrases/phrase.dart';
 import 'package:todaygoodwords/phrases/phrase_bloc.dart';
 
 class ApplicationRunner {
   final Widget _app;
   
-  ApplicationRunner(PhraseBloc phraseBloc) : _app = TodayGoodWords(phraseBloc: phraseBloc);
+  ApplicationRunner(PhraseBloc phraseBloc, PhraseThemeBloc phraseThemeBloc)
+      : _app = TodayGoodWords(phraseBloc: phraseBloc, phraseThemeBloc: phraseThemeBloc);
   
-  Future<void> startApp(WidgetTester tester) async {
+  Future<void> startApp(final WidgetTester tester) async {
     await tester.pumpWidget(_app);
     await tester.pump(Duration.zero);
   }
@@ -18,9 +20,21 @@ class ApplicationRunner {
     expect(find.text('불러오는 중입니다.'), findsOneWidget);
   }
 
-  void displayPhrase(final Phrase phrase) {
-    expect(find.text(phrase.name), findsOneWidget);
-    expect(find.textContaining(phrase.text), findsOneWidget);
+  void displayPhrase(final WidgetTester tester, final Phrase phrase, final PhraseTheme phraseTheme) {
+    final nameFinder = find.text(phrase.name);
+    final textFinder = find.textContaining(phrase.text);
+
+    expect(nameFinder, findsOneWidget);
+    expect(textFinder, findsOneWidget);
+
+    var name = tester.firstWidget(nameFinder) as Text;
+    var text = tester.firstWidget(textFinder) as Text;
+
+    expect(name.style?.fontSize, phraseTheme.name.fontSize);
+    expect(text.style?.fontSize, phraseTheme.text.fontSize);
+
+    expect(name.style?.color, Color(phraseTheme.name.color));
+    expect(text.style?.color, Color(phraseTheme.text.color));
   }
 
   void displayFailure() {
--- test/end_to_end/end_to_end_test.dart
+++ test/end_to_end/end_to_end_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todaygoodwords/phrase_themes/phrase_theme.dart';
 import 'package:todaygoodwords/phrases/phrase.dart';
 import 'package:todaygoodwords/phrases/phrase_bloc.dart';
 
@@ -6,30 +7,39 @@ import 'application_runner.dart';
 import 'fake_phrase_repository.dart';
 
 void main() {
-  goodWordTestGroup();
-}
-
-void goodWordTestGroup() {
-  group('Good Words Test', () {
+  group('E2E tests', () {
     late ApplicationRunner app;
 
     testWidgets('display loading message when phrase is loading', (tester) async {
-      app = ApplicationRunner(PhraseBloc(phraseRepositoryReadingNothing()));
+      var phraseBloc = PhraseBloc(phraseRepositoryReadingNothing());
+      var phraseThemeBloc = PhraseThemeBloc(phraseThemeRepositoryReadingNothing());
+
+      app = ApplicationRunner(phraseBloc, phraseThemeBloc);
+
       await app.startApp(tester);
       app.displayLoading();
     });
 
     testWidgets('display failed message when phrase is not loaded', (tester) async {
-      app = ApplicationRunner(PhraseBloc(phraseRepositoryOccurErrorWhileReading()));
+      var phraseBloc = PhraseBloc(phraseRepositoryOccurErrorWhileReading());
+      var phraseThemeBloc = PhraseThemeBloc(phraseThemeRepositoryOccurErrorWhileReading());
+
+      app = ApplicationRunner(phraseBloc, phraseThemeBloc);
+
       await app.startApp(tester);
       app.displayFailure();
     });
 
     testWidgets('display correct phrase when phrase is loaded', (tester) async {
       var phrase = Phrase('name', 'text');
-      app = ApplicationRunner(PhraseBloc(phraseRepositoryReadingOne(phrase)));
+      var phraseTheme = PhraseTheme();
+      var phraseBloc = PhraseBloc(phraseRepositoryReadingOne(phrase));
+      var phraseThemeBloc = PhraseThemeBloc(phraseThemeRepositoryReadingOne(phraseTheme));
+
+      app = ApplicationRunner(phraseBloc, phraseThemeBloc);
+
       await app.startApp(tester);
-      app.displayPhrase(phrase);
+      app.displayPhrase(tester, phrase);
     });
   });
 }
