10360064533aee908b87003090b05813c2284412
Lee-Null <nullist.null@gmail.com>
2021-12-25 01:21:37 +0900

    fix covera

--- coverage_helper.sh
+++ coverage_helper.sh
@@ -13,8 +13,9 @@ if [ $option = "--create" ]; then
   # create coverage_helper_test.dart that imports all lib files
   echo "// Helper file to make coverage work for all dart files\n" > $file
   echo "// ignore_for_file: unused_import" >> $file
+  echo "import 'package:flutter_test/flutter_test.dart';" >> $file
   find lib -name '*.dart' | cut -c4- | awk -v package=$package '{printf "import '\''package:%s%s'\'';\n", package, $1}' >> $file
-  echo "\nvoid main(){}" >> $file
+  echo "void main(){ test('empty test', () {}); }" >> $file
 elif [ $option = "--clean" ]; then
   # delete empty test directory
   rm $file
